39bb7f49d1bb24628cf8d8c649fd8de3257e2d9f
KGB-8375 <27stopittj@gmail.com>
2021-07-17 14:50:36 -0400

    Re-organized UI

--- app/ui.R
+++ app/ui.R
@@ -3,62 +3,42 @@ library(leaflet)
 library(plotly)
 library(shinythemes)
 
-# Define UI for application that draws a histogram
-shinyUI(navbarPage( "CovidEye", windowTitle = "CovidEye",
-                    theme = shinytheme("cosmo"),
-    ## DASHBOARD PAGE ##########################################################
-    tabPanel("Dashboard",
-        tags$h1("The Commonwealth of Virginia"),
-        htmlOutput("db_stats"),
-        fluidRow(
-            column(9, leafletOutput("db_map")),
-            column(3, wellPanel(
-                htmlOutput("db_date_ui"),
-                radioButtons("db_mode", "Show", selected = "cases",
-                             choices = list("Population" = "pop",
-                                            "Total Cases" = "cases",
-                                            "Hospitalizations" = "hosp",
-                                            "Deaths" = "deaths")),
-                checkboxInput("db_pop_adj", "Adjust for Population",
-                              value = TRUE))
-            )
-        ),
-        tags$h2("Daily Rates"),
-        htmlOutput("db_date_rng_ui"),
-        plotlyOutput("db_rates"),
-        tags$h2("County with Most Cases (Population Adjusted)"),
-        plotlyOutput("db_highest_cases"),
-        tags$h2("County with Highest Daily Rate (Population Adjusted)"),
-        plotlyOutput("db_highest_rates")
-    ),
-    ## BY COUNTRY PAGE #########################################################
-    tabPanel("By County",
-        "This page is intentionally blank."),
-    ## DEMOGRAPHICS PAGE #######################################################
-    tabPanel("Demographics",
-        tags$h1("Who COVID-19 is affecting the most"),
-        fluidRow(
-            column(6, wellPanel(htmlOutput("demo_date_ui"),
-                      radioButtons("demo_mode","Show", selected = "cases",
-                                   choices = list("Cases" = "cases",
-                                                  "Hospitilizations" = "hosp",
-                                                  "Deaths" = "deaths")),
-                      checkboxInput("demo_pop_adj", "Adjust for Population",
-                                    value = TRUE)),),
-            column(6, tags$h2("Age Groups", align = 'center'),
-                      plotlyOutput("demo_age"))
-        ),
-        fluidRow(
-            column(6, align = 'center', tags$h2("Sex"),
-                      plotlyOutput("demo_sex", width = '50%')),
-            column(6, tags$h2("Race", align = 'center'),
-                      plotlyOutput("demo_race"))
-        )
+# Scripts
+source("ui/dashboard.R",    local = TRUE)
+source("ui/byCounty.R",     local = TRUE)
+source("ui/demographics.R", local = TRUE)
 
-    ),
-    ## ABOUT PAGE ##############################################################
-    tabPanel("About",
-        includeMarkdown("./about.md")),
-    tabPanel("Disclaimer",
-        includeMarkdown("./disclaimer.md"))
-))
+# Define UI for application that draws a histogram
+shinyUI(
+    navbarPage(
+        title       = "CovidEye",
+        windowTitle = "CovidEye",
+        theme       = shinytheme("cosmo"),
+        
+        # Dashboard
+        tabPanel(
+            title = "Dashboard",
+            makeDashboard()
+        ),
+        # By County
+        tabPanel(
+            title = "By County",
+            makeByCounty()
+        ),
+        # Demographics
+        tabPanel(
+            title = "Demographics",
+            makeDemographics()
+        ),
+        # About
+        tabPanel(
+            title = "About",
+            includeMarkdown("./about.md")
+        ),
+        # Disclaimer
+        tabPanel(
+            title = "Disclaimer",
+            includeMarkdown("./disclaimer.md")
+        )
+    )
+)
---
+++ app/ui/byCounty.R
@@ -0,0 +1,12 @@
+# BY COUNTY UI
+
+makeByCounty <- function() {
+  return(
+    div(
+      #title
+      h1("Information for your area"),
+      # Placeholder
+      "This page is intentionally blank."
+    )
+  )
+}
